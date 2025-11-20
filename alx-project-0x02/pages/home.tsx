a9934048358efe06f1350b4cdbea42289a797146
Davcid1572 <ifejiofordavid1572@gmail.com>
2025-11-20 11:30:46 +0100

    Creating and rendering the Card component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Link from "next/link";
+import Card from "@/components/common/Card";
 
 const home = () => {
   return (
     <div>
       <h1>Welcome to home</h1>
       <Link href="/about">Go to About</Link>
+      <Card title="Sample Card" content="This is a sample card component." />
+      <Card
+        title="Another Card"
+        content="This is another sample card component."
+      />
     </div>
   );
 };
